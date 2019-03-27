57b72f23586733925424e3473408e7125502bb61
andri <andrio@gmail.com>
2019-03-27 15:26:04 +0000

    Camping page v1

---
+++ src/Assets/Camping/index.js
@@ -0,0 +1,36 @@
+import CoverPhoto from "./camping.jpg";
+
+const InformationBoxPrice = `Day Fees`;
+
+const Price = [
+  {
+    Title: `Single`,
+    Price: `3.200`
+  },
+  {
+    Title: `Couples`,
+    Price: `5.000`
+  }
+];
+
+const TextInformation = [
+  {
+    Title: `Information`,
+    Text: `The camping site in Uthlid is well located close to the main road and in walking distance from the restaurant, swimming pool and golf course.  High trees give shelter for rain and wind.  In the middle of the Golden Circle, 10 km. from Geysir.
+
+    The camping site is Open all the year.  During winter time vehicles are parked on the parking site and grass field outside the service center.
+    `
+  }
+];
+
+const Includes = [`Toilets`, `Showers`, `hot tubs`];
+
+const Info = {
+  CoverPhoto,
+  TextInformation,
+  Includes,
+  InformationBoxPrice,
+  Price
+};
+
+export default Info;
--- src/components/Camping/CampingContainer.jsx
+++ src/components/Camping/CampingContainer.jsx
@@ -3,22 +3,16 @@ import styled from "styled-components";
 import BlackbarInformation from "../Reusable/BlackbarInformation/BlackbarInformation";
 import Header from "../SinglePageCottage/Header";
 import Layout from "../Reusable/Layout";
-import CampingBackground from "../../Assets/Camping/camping.jpg";
+import TextContainer from "../SinglePageCottage/TextContainer";
+
+// Import Assets
+import Info from "../../Assets/Camping/index";
 
 const CampingContainer = () => (
   <Layout>
-    <Header CoverPhoto={CampingBackground} />
-    <CampingInformation />
+    <Header CoverPhoto={Info.CoverPhoto} />
+    <TextContainer {...Info} />
   </Layout>
 );
 
 export default CampingContainer;
-
-const CampingInformation = styled.div`
-  height: 100%;
-  width: 60%;
-  display: flex;
-  flex-wrap: wrap;
-  background-color: transparent;
-  justify-content: space-around;
-`;
