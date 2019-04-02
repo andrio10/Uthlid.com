81763168eb67d12340e7acf4b3851effc97dede5
Megadurgurfishbait <gummehh@gmail.com>
2019-04-02 14:53:13 +0000

    Restaurant Menu

---
+++ src/Assets/Restaurant/MenuInfo.js
@@ -0,0 +1,91 @@
+const Pizza = [
+  { Title: `Margharita `, Ingred: `Tomato sauce, cheese`, Price: `1.590 ISK` },
+  {
+    Title: `Pepperoni/Ham`,
+    Ingred: `Tomato sauce, cheese, pepperoni/ham`,
+    Price: `1.790 ISK`
+  },
+  {
+    Title: `Toscana `,
+    Ingred: `Tomato sauce, cheese, pepperoni, Jalapeno, Black olives, mushrooms, pineapple, garlic`,
+    Price: `1.990 ISK`
+  },
+  {
+    Title: `Prima Donna`,
+    Ingred: `Tomato sauce, cheese, cream cheese, pepperoni, ham, bacon, oregano`,
+    Price: `1.990 ISK`
+  },
+  {
+    Title: `Vegetarian`,
+    Ingred: `Tomato sauce, cheese, tomatoes, mushrooms, corn,red onion`,
+    Price: `1990 ISK`
+  }
+];
+
+const Hamburgers = [
+  {
+    Title: `Uthlid Burger`,
+    Ingred: `Beef, lettuce, tomatoes, cucumber, onion, cheese`,
+    Price: `2.000 ISK`
+  },
+  {
+    Title: `Lamb burger`,
+    Ingred: `Lamb meat, lettuce, tomatoes, cucumber, onion, cheese`,
+    Price: `2.000 ISK`
+  },
+  {
+    Title: `Veg burger`,
+    Ingred: `Veggieburger, lettuce, tomatoes, cucumber, onion, cheese`,
+    Price: `2.000 ISK`
+  },
+  { Title: `Extras`, Ingred: `Bacon, egg, mushooms, blue cheese, jalapeno` }
+];
+
+const Sandwiches = [
+  { Title: `Ham sandwich`, Ingred: `ham, cheese, sause`, Price: `900 ISK` },
+  {
+    Title: `Vegeterian sandwich`,
+    Ingred: `double cheese, tomatoes, mushrooms, cucumber, onion, sauce`,
+    Price: `900 ISK`
+  },
+  {
+    Title: `Rettar sandwich`,
+    Ingred: `salami, cheese, fried egg, french fries, sauce`,
+    Price: `1.400 ISK`
+  },
+  { Title: `Soup of the day`, Price: `1.100 ISK` }
+];
+
+const Salads = [
+  {
+    Title: `Feta cheese salad`,
+    Ingred: `lettuce, vegetables and fruits, feta cheese`,
+    Price: `2.000 ISK`
+  },
+  {
+    Title: `Lamb salad`,
+    Ingred: `lamb, lettuce, variety of vegetables, feta cheese`,
+    Price: `2.500 ISK`
+  },
+  {
+    Title: `Chicken salad`,
+    Ingred: `chicken, lettuce, variety of vegetables, feta cheese`,
+    Price: `2.500 ISK`
+  }
+];
+
+const Other = [
+  {
+    Title: `Lamb meat lasagna`,
+    Ingred: `with baked baguette`,
+    Price: `3.000 ISK`
+  },
+  {
+    Title: `Traditional Icelandic Plate`,
+    Ingred: `selection of traditional Icelandic food`,
+    Price: `2.500 ISK`
+  }
+];
+
+export default {Pizza, Hamburgers, Sandwiches, Salads, Other};
+
